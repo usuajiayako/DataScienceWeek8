021c19691045764c39ea5b1c546cf0ae7df16025
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-11-22 15:56:49 +0000

    finish streamlit sidebar selection

--- supervised_mini_project/streamlit_mushrooms.py
+++ supervised_mini_project/streamlit_mushrooms.py
@@ -9,11 +9,8 @@ import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report
 
-import pickle
-
-data = pd.read_csv("mushrooms.csv")
+data = pd.read_csv("/Users/ayakobland/DataScienceBootCamp8/supervised_mini_project/mushrooms.csv")
 
 encoder = OneHotEncoder()
 
@@ -28,10 +25,9 @@ def one_hot_encoder():
 
     return new_data
 
-new_data = one_hot_encoder():
+new_data = one_hot_encoder()
 new_data.drop("class_p", axis = 1, inplace = True)
 new_data.rename(columns = {"class_e": "edibility"}, inplace = True)
-new_data
 
 X = new_data.iloc[:, 1:]
 y = new_data.iloc[:, 0]
@@ -42,12 +38,58 @@ model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-y_pred
 
-cm = confusion_matrix(y_test, y_pred)
 
-sns.heatmap(cm/np.sum(cm), annot = True, cmap = "Blues", fmt = ".2%")
-plt.xlabel("Prediction")
-plt.show()
+# %%
+########################### STREAMLIT ########################################
+import streamlit as st
 
-print(classification_report(y_test, y_pred))
+def main():
+    st.title("Mushroom Edibility Test")
+    st.sidebar.title("Feature/Category Selection")
+    st.image("mushroom.png")
+main()
+
+cap_shape = st.sidebar.selectbox("Cap shape", ["bell", "conical", "convex", "flat", "knobbed", "sunken"])
+cap_surface = st.sidebar.selectbox("Cap surface", ["fibrous", "grooves", "scaly", "smooth"])
+cap_color = st.sidebar.selectbox("Cap color", ["brown", "buff", "cinnamon", "gray" , "green", "pink", "purple", "red", "white", "yellow"])
+bruises = st.sidebar.selectbox("Bruises", ["yes", "no"]) 
+odor = st.sidebar.selectbox("Odor", ["almond", "anise", "creosote", "fishy", "foul", "musty", "none", "pungent", "spicy"])
+gill_attachment = st.sidebar.selectbox("Gill attachment", ["attached", "descending", "free", "notched"])
+gill_spacing = st.sidebar.selectbox("Gill spacing", ["close", "crowded", "distant"])
+gill_size = st.sidebar.selectbox("Gill size", ["broad", "narrow"])
+gill_color = st.sidebar.selectbox("Gill_color", ["black", "brown", "buff", "chocolate", "gray", "green", "orange", "pink", "purple", "red", "white", "yellow"])
+stalk_shape = st.sidebar.selectbox("Stalk shape", ["enlarging", "tapering"])
+stalk_root = st.sidebar.selectbox("Stalk root", ["bulbous", "club", "cup", "equal", "rhizomorphs", "rooted", "missing"])
+stalk_surface_above_ring = st.sidebar.selectbox("Stalk surface above ring", ["fibrous", "scaly", "silky", "smooth"])
+stalk_surface_below_ring = st.sidebar.selectbox("Stalk surface below ring", ["fibrous", "scaly", "silky", "smooth"])
+stalk_color_above_ring = st.sidebar.selectbox("Stalk color above ring", ["brown", "buff", "cinnamon", "gray", "orange", "pink", "red", "white", "yellow"])
+stalk_color_below_ring = st.sidebar.selectbox("Stalk color below ring", ["brown", "buff", "cinnamon", "gray", "orange", "pink", "red", "white", "yellow"])
+veil_type = st.sidebar.selectbox("Veil type", ["partial", "universal"])
+veil_color = st.sidebar.selectbox("Veil color", ["brown", "orange", "white", "yellow"])
+ring_number = st.sidebar.selectbox("Ring number", ["none", "one", "two"])
+ring_type = st.sidebar.selectbox("Ring type", ["cobwebby", "evanescent", "flaring", "large", "none", "pendant", "sheathing", "zone"])
+spore_print_color = st.sidebar.selectbox("Spore print color", ["black", "brown", "buff", "chocolate", "green", "orange", "purple", "white", "yellow"])
+population = st.sidebar.selectbox("Population", ["abundant", "clustered", "numerous", "scattered", "several", "solitary"])
+habitat = st.sidebar.selectbox("Habitat", ["grasses", "leaves", "meadows", "paths", "urban", "waste", "woods"])
+
+result = [cap_shape, cap_surface, cap_color, bruises, odor, gill_attachment, gill_spacing, gill_size, gill_color, stalk_shape, stalk_root, stalk_surface_above_ring, stalk_surface_below_ring, stalk_color_above_ring, stalk_color_below_ring, veil_type, veil_color, ring_number, ring_type, spore_print_color, population, habitat]
+
+# Creating test dataframe
+test_data = pd.DataFrame(columns = new_data.columns, index = ["Test data"])
+test_data = test_data.fillna(0)
+
+# Filling the result
+if cap_shape == "bell":
+    test_data["cap-shape_b"] = 1
+elif
+
+
+
+
+
+
+st.write(test_data)
+st.write(result[0])
+
+
